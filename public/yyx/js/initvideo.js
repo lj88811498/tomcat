798e1d3721a62d497a464d7c1ae101e7ada8123e
lijiang <450416064@qq.com>
2018-12-07 18:04:01 +0800

    添加静态资源

---
+++ public/yyx/js/initvideo.js
@@ -0,0 +1,45 @@
+ init();
+ 
+ function init() {
+	
+	//需要自动播放就在vedio上添加 autoplay='autoplay'
+	var pre = "<li><div><video controls='controls'  width='250' height='310'><source src='http://47.104.162.57:8083/";
+        var fix = ".mp4' type='video/mp4' /> Your browser does not support the video tag.</video></div></li>";            
+	 
+	//第一页
+	var temp = '';
+
+	for (i = 1; i<= 8; i ++) {
+
+		temp  += pre + i + fix;
+	
+	}
+	
+	$("#main").html(temp); 
+
+
+	//第二页
+	var temp2 = '';
+
+	for (i = 9; i<= 18; i ++) {
+
+		temp2  += pre + i + fix;
+	
+	}
+	
+	$("#spirit").html(temp2); 
+
+	//第三页
+	var temp3 = '';
+
+	for (i = 19; i<= 26; i ++) {
+
+		temp3  += pre + i + fix;
+	
+	}
+	
+	$("#magic").html(temp3); 
+
+	
+	 
+ }
